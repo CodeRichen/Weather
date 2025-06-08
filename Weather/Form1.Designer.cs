56ec8422c2fa0d416bc4cad4288ae658ada1098d
CodeRichen <lichengxing269@gmail.com>
2025-06-08 19:23:09 +0800

    before

--- Weather/Form1.Designer.cs
+++ Weather/Form1.Designer.cs
@@ -32,15 +32,20 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 15F);
-            this.label1.Location = new System.Drawing.Point(729, 27);
+            this.label1.Location = new System.Drawing.Point(97, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(145, 40);
             this.label1.TabIndex = 2;
@@ -65,17 +70,61 @@
             this.panel1.Size = new System.Drawing.Size(2160, 1128);
             this.panel1.TabIndex = 4;
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 13F);
+            this.textBox1.Location = new System.Drawing.Point(1093, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1104, 186);
+            this.textBox1.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(674, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(350, 222);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.button1.Location = new System.Drawing.Point(78, 1374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 90);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 15F);
+            this.button2.Location = new System.Drawing.Point(564, 1374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 90);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "finish";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2250, 1412);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,6 +134,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Weather/Form1.cs
+++ Weather/Form1.cs
@@ -5,22 +5,48 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net;
+
 
 namespace Weather
 {
     public partial class Form1 : Form
     {
         private readonly string apiKey = "a0f224b44a20b00528ca4f36aa12a43b";
-
+        private const string UnsplashAccessKey = "QQxvNvDKTnvuWKAD8ToLAswiqTqih3a_3bNCi8rDK7A"; 
+        private List<(string city, string countryCode)> cityList = new List<(string, string)>();
+        private Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
             this.Load += Form1_Load;
+            LoadCitiesFromFile("cities1000.txt");
         }
 
         private Point mouseDownLocation;
         private bool isDragging = false;
-
+        private void LoadCitiesFromFile(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (var line in lines)
+                {
+                    var parts = line.Split('\t');
+                    if (parts.Length > 8)
+                    {
+                        string city = parts[1];
+                        string countryCode = parts[8];
+                        cityList.Add((city, countryCode));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("讀取城市檔案錯誤：" + ex.Message);
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Width = 1200;
@@ -29,6 +55,7 @@ namespace Weather
 
             pictureBox1.Image = Image.FromFile("world_map.jpg");
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.Dock = DockStyle.None;
             pictureBox1.Width = 1000;
             pictureBox1.Height = 600;
@@ -114,7 +141,7 @@ namespace Weather
 
         private async void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
-            double lon = (e.X / (double)pictureBox1.Width) * 360 - 179.5;
+            double lon = (e.X / (double)pictureBox1.Width) * 360 - 179;
             double lat = 90.5 - (e.Y / (double)pictureBox1.Height) * 180;
 
             var (zhName, enName) = await GetCityNameByCoordinatesAsync(lat, lon);
@@ -133,7 +160,17 @@ namespace Weather
                 }
             }
             string weatherInfo = await GetWeatherAsync(enName);
-                
+            string summary = await GetCitySummaryAsync(zhName, enName);
+            textBox1.Text = summary;
+
+            Image cityImg = await GetCityImageAsync(enName);
+            if (cityImg != null)
+                pictureBox2.Image = cityImg;
+            else
+                pictureBox2.Image = null;
+
+
+
             label1.Text = $"城市：{zhName} ({enName})\n{weatherInfo}\n";
 
         }
@@ -163,7 +200,7 @@ namespace Weather
 
                     if (!responseZh.IsSuccessStatusCode || !responseEn.IsSuccessStatusCode)
                     {
-                        return ($"查詢城市失敗：HTTP {responseZh.StatusCode} / {responseEn.StatusCode}", "");
+                        return ($"查詢城市失敗", "");
                     }
 
                     string jsonZh = await responseZh.Content.ReadAsStringAsync();
@@ -176,7 +213,7 @@ namespace Weather
                 }
                 catch (Exception ex)
                 {
-                    return ($"查詢城市失敗：{ex.Message}", "");
+                    return ($"查詢城市失敗", "");
                 }
             }
         }
@@ -200,7 +237,157 @@ namespace Weather
                 }
                 catch (Exception ex)
                 {
-                    return $"取得{city}天氣資訊失敗：{ex.Message}";
+                    return $"取得{city}天氣資訊失敗";
+                }
+            }
+        }
+        private async Task<string> GetCitySummaryAsync(string zhName, string enName)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // 先嘗試中文維基百科
+                    string zhUrl = $"https://zh.wikipedia.org/api/rest_v1/page/summary/{Uri.EscapeDataString(zhName)}";
+                    HttpResponseMessage zhRes = await client.GetAsync(zhUrl);
+
+                    if (zhRes.IsSuccessStatusCode)
+                    {
+                        string zhContent = await zhRes.Content.ReadAsStringAsync();
+                        JObject zhJson = JObject.Parse(zhContent);
+                        string zhExtract = zhJson["extract"]?.ToString();
+
+                        if (!string.IsNullOrEmpty(zhExtract)) return zhExtract;
+                    }
+
+                    // 如果中文沒資料，嘗試英文摘要 + 翻譯
+                    string enUrl = $"https://en.wikipedia.org/api/rest_v1/page/summary/{Uri.EscapeDataString(enName)}";
+                    HttpResponseMessage enRes = await client.GetAsync(enUrl);
+
+                    if (!enRes.IsSuccessStatusCode) return "找不到該城市的簡介資料。";
+
+                    string enContent = await enRes.Content.ReadAsStringAsync();
+                    JObject enJson = JObject.Parse(enContent);
+                    string enExtract = enJson["extract"]?.ToString();
+
+                    if (string.IsNullOrEmpty(enExtract)) return "找不到該城市的簡介資料。";
+
+                    // 使用簡單的 DeepL 或 OpenAI 翻譯（此處用 Google Translate 網址示意）
+                    //string translated = await TranslateToChinese(enExtract);
+                    //return translated;
+                    return enExtract;
+                }
+                catch (Exception ex)
+                {
+                    return $"查詢城市簡介失敗";
+                }
+            }
+        }
+        private async Task<Image> GetCityImageAsync(string enName)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string url = $"https://en.wikipedia.org/api/rest_v1/page/summary/{Uri.EscapeDataString(enName)}";
+                    string response = await client.GetStringAsync(url);
+                    JObject json = JObject.Parse(response);
+                    string imgUrl = json["thumbnail"]?["source"]?.ToString();
+
+                    if (!string.IsNullOrEmpty(imgUrl))
+                    {
+                        var stream = await client.GetStreamAsync(imgUrl);
+                        return Image.FromStream(stream);
+                    }
+                }
+                catch { }
+                return null;
+            }
+        }
+
+        private (double lat, double lon)? player1Coords = null;
+        private (double lat, double lon)? player2Coords = null;
+        private int currentPlayer = 1; // 玩家1先點
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //TODO:按一下這個開始遊戲 先把地圖 變成預設載入的大小 鎖定地圖讓他不能夠改大小
+            //TODO:2個使用者可以拖曳某個物品到地圖上面 
+            if (cityList.Count == 0)
+            {
+                MessageBox.Show("城市清單為空！");
+                return;
+            }
+
+            var (city, countryCode) = cityList[rand.Next(cityList.Count)];
+            string countryName = await GetCountryNameFromCode(countryCode);
+            if (countryName == null)
+            {
+                MessageBox.Show($"找不到國家代碼 {countryCode}");
+                return;
+            }
+
+            string imageUrl = await GetImageFromUnsplash(countryName);
+            if (imageUrl != null)
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create(imageUrl);
+                    using (WebResponse response = request.GetResponse())
+                    using (Stream stream = response.GetResponseStream())
+                    {
+                        pictureBox2.Image = System.Drawing.Image.FromStream(stream);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("載入圖片失敗：" + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("找不到圖片！");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //TODO:按下這個就可以 對答案 把兩位使用者選的地點和 正確答案的地點標示出來 誰比較接近誰就獲勝畫出 正確的點到2個地點的線 
+        }
+        private async Task<string> GetCountryNameFromCode(string code)
+        {
+            string url = $"https://restcountries.com/v3.1/alpha/{code}";
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string json = await client.GetStringAsync(url);
+                    JArray arr = JArray.Parse(json);
+                    return arr[0]?["name"]?["common"]?.ToString();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        private async Task<string> GetImageFromUnsplash(string query)
+        {
+            string url = $"https://api.unsplash.com/photos/random?query={Uri.EscapeDataString(query)}&client_id={UnsplashAccessKey}";
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string json = await client.GetStringAsync(url);
+                    JObject obj = JObject.Parse(json);
+                    return obj["urls"]?["regular"]?.ToString();
+                }
+                catch
+                {
+                    return null;
                 }
             }
         }
